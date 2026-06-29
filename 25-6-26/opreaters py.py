c4964de7085a263f0dad29e0e866a09be10613d0
vatsal vaghela <creativewithvaghela.vatsal@gmail.com>
2026-06-29 17:16:56 +0500

    Add files via upload

---
+++ 25-6-26/opreaters py.py
@@ -0,0 +1,124 @@
+#python opreaters veriable and data types
+
+'''
+1-arithmetic opreater
+
+2-assignment opreater
+
+3-comperison opreater 
+
+4-logical opreater
+
+5-bitwise opreater
+
+6-type conversion
+'''
+
+a=10
+
+b=20
+
+print("addition;",a+b)
+
+print("substraction",a-b)
+
+print("multiplication;",a*b)
+
+print("division;",a/b)
+
+print("modulus;",a%b)
+
+print("exponentiation;",a**b)
+
+#assignment operator 
+
+x=5
+
+y=3
+
+x+=y
+
+x-=y
+
+x/=y
+
+x%=y
+
+x**=y
+
+x//=y
+
+#comparision operator
+
+x=10
+
+y=10
+
+print(x==y)
+
+print(x!=y)
+
+print(x<y)
+
+print(x<=y)
+
+print(x>y)
+
+print(x>=y)
+
+x=y
+
+print(x)
+
+#logical operator (and,or,not)
+
+x=True
+y=False
+z=False
+
+print(x and y)
+
+print(y and z)
+
+print(x or y)
+
+print(y or z)
+
+print(not x)
+
+print(not y)
+
+#type conversion
+
+'''
+
+int()
+
+flot()
+
+str()
+
+tuple()
+
+list()
+
+set()
+
+dict()
+
+'''
+
+num_str='123'
+print(type(num_str))
+
+num_int=int(num_str)
+print(type(num_int))
+
+#multiple assignment
+
+x,y,z=10,20,30
+
+print(x,y,z)
+
+a=20
+b=30
