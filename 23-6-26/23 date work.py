ce628aca86f966e5c0b0a02e69e35916bdfc7fa0
vatsal vaghela <creativewithvaghela.vatsal@gmail.com>
2026-06-29 17:28:25 +0500

    Add files via upload

---
+++ 23-6-26/23 date work.py
@@ -0,0 +1,7 @@
+print("hello python")
+print("my name is vatsal vaghela")
+print("my age is a 18")
+print("to day i am exited larn to a python")
+print("this day i am exprince to python its very cool softwer")
+
+ 
